1d7745f2b7588dae6cf0c755e143da36834f904a
Menno Spijker <menno@socialdeal.nl>
2024-01-12 08:47:47 +0100

    analytics error

--- app/Http/Controllers/AnalyticsController.php
+++ app/Http/Controllers/AnalyticsController.php
@@ -19,15 +19,22 @@ class AnalyticsController extends Controller
         $eventName = $request->input('event_name');
         $parameters = $request->input('parameters', []);
 
-        // Track the event in the database
-        AnalyticsEvent::create([
-            'uuid' => $uuid,
-            'event_name' => $eventName,
-            'parameters' => $parameters,
-        ]);
+        try {
+            // Check if 'parameters' is an array
+            if (!is_array($parameters)) {
+                throw new \Exception('Parameters must be an array.');
+            }
 
-        // You can add additional logic or responses here based on your requirements
+            // Track the event in the database
+            AnalyticsEvent::create([
+                'uuid' => $uuid,
+                'event_name' => $eventName,
+                'parameters' => $parameters,
+            ]);
 
-        return response()->json(['message' => 'Event tracked successfully']);
+            return response()->json(['message' => 'Event tracked successfully']);
+        } catch (\Exception $e) {
+            return response()->json(['error' => $e->getMessage()], 400);
+        }
     }
 }
